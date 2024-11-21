e46220d5781921a7eecca82391b3d46b27f80cee
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2024-11-21 20:57:04 +0530

    27 version

--- src/middleware.js
+++ src/middleware.js
@@ -2,14 +2,8 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   const url = req.nextUrl.clone();
-  const authCookie =
-    process.env.NODE_ENV === "production"
-      ? "__Secure-next-auth.session-token"
-      : "next-auth.session-token";
-
-  const isAuthenticated = req.cookies.get(authCookie)?.value;
-
-  console.log("Middleware Auth Cookie:", isAuthenticated); // Debugging
+  const authCookie = "next-auth.session-token"; // Define cookie name here
+  const isAuthenticated = req.cookies.get(authCookie);
 
   // Redirect authenticated users away from login page
   if (url.pathname === "/login" && isAuthenticated) {
@@ -23,10 +17,11 @@ export async function middleware(req) {
     return NextResponse.redirect(url);
   }
 
-  // Allow other requests to proceed
+  // Proceed with the request for other routes
   return NextResponse.next();
 }
 
+// Apply middleware only to login and dashboard routes
 export const config = {
   matcher: ["/login", "/dashboard"],
 };
